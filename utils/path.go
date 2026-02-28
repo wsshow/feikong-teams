72275d654096de03213e171aa2463e37719a27bf
ws <wsshow@foxmail.com>
2026-02-28 09:25:52 +0800

    feat(utils): 新增路径相关通用函数

---
+++ utils/path.go
@@ -0,0 +1,25 @@
+package utils
+
+import "os"
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func EnsureDir(path string) error {
+	_, err := os.Stat(path)
+	if err == nil {
+		return nil
+	}
+	if os.IsNotExist(err) {
+		return os.MkdirAll(path, 0755)
+	}
+	return err
+}
