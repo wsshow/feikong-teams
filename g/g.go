4eb5147bcd105add74a9bad472fa9f5f49dfaf33
ws <wsshow@foxmail.com>
2026-03-10 13:13:06 +0800

    chore(g): 添加注释

--- g/g.go
+++ g/g.go
@@ -2,10 +2,7 @@ package g
 
 import (
 	"fkteams/common"
-	"fkteams/config"
-	"fkteams/log"
 	"fkteams/memory"
-	"fkteams/utils"
 )
 
 // Cleaner 资源清理器，支持注册多个清理函数，在退出前统一执行
@@ -13,15 +10,3 @@ var Cleaner = common.NewResourceCleaner()
 
 // MemManager 全局记忆管理器
 var MemManager *memory.Manager
-
-// Log 全局日志记录器
-var Log = NewLog()
-
-func NewLog() *log.Log {
-	utils.NotExistToMkdir("./log")
-	cfg, _ := config.Get()
-	if cfg.Server.LogLevel == "" {
-		cfg.Server.LogLevel = "info"
-	}
-	return log.New("fkteams", cfg.Server.LogLevel)
-}
